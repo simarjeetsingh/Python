728d96e5bc4c7dbef5f8517a2dc31231d9a293db
simarjeetsingh <simarjeetsingh2011@gmail.com>
2017-01-29 00:03:58 +0100

    Update Ej_01.py

--- Ejercicios Python/Ej_01.py
+++ Ejercicios Python/Ej_01.py
@@ -2,7 +2,7 @@
 oportuno. A continuación escribe en pantalla el número leído, el doble del número leído y
 el triple del mismo.'''
 
-a = int(input("Introduzca un numero: "))
-print('El numero introducido es:', a)
-print('El doble del numero introducido es:', a * 2)
-print('El triple del numero introducido es:', a * 3)
+num = int(input("Introduzca un numero: "))
+print('El numero introducido es:', num)
+print('El doble del numero introducido es:', num * 2)
+print('El triple del numero introducido es:', num * 3)
